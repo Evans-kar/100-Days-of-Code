70a686adf7a917bad41843057e6189098746d5a1
Evans Karuri <duneevanson6@gmail.com>
2025-10-22 16:50:25 -0400

    practiced assignment operators

--- operators.js
+++ operators.js
@@ -6,3 +6,7 @@ console.log (x);
 let text1 ="Today was a good day"; //string
 text1 += "since the sunny was out wish I knew how to swim!"; //+= is used for string concatenation
 console.log(text1);
+//assignment operators
+console.log(x%a);
+console.log(x**a);
+console.log(x+=a);
