1cfa23ca6bdd699a1ce34d08d1e946fb1c99471a
Evans Karuri <duneevanson6@gmail.com>
2025-10-22 16:28:45 -0400

    arithmetic operators

---
+++ operators.js
@@ -0,0 +1,4 @@
+//arithmetic operators
+let a = 3;   //declared variable a and initialized it 
+let x = (100 + 50) * a; // arithmetic operators used for addition and multiplication
+console.log (x);
