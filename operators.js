f02b3d5239ee97bfdc8441e936c7e3fde855e665
Evans Karuri <duneevanson6@gmail.com>
2025-10-22 16:42:47 -0400

    string concatenation operator

--- operators.js
+++ operators.js
@@ -2,3 +2,7 @@
 let a = 3;   //declared variable a and initialized it 
 let x = (100 + 50) * a; // arithmetic operators used for addition and multiplication
 console.log (x);
+//string concatenation 
+let text1 ="Today was a good day"; //string
+text1 += "since the sunny was out wish I knew how to swim!"; //+= is used for string concatenation
+console.log(text1);
