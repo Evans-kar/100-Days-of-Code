c27550098cd920203b8a948df71793c1a75c9232
Evans Karuri <duneevanson6@gmail.com>
2025-10-22 18:16:41 -0400

    Practiced conditional statements

--- control_flow.js
+++ control_flow.js
@@ -1,6 +1,24 @@
 //conditional if statements execute code when the condition being tested is true
-let hours = 10
+let hours = 12;
 if (hours < 18) {
     greetings = "Guten Morgen!";
 }
 console.log (greetings);
+//conditional else statement executes if the condition is false
+let age = 22;
+if (age < 21) {
+    text = "Drinking is not allowed to any persons under the age of 21!";
+} else {
+    text = "Some bottles for you mate!!";
+}
+console.log (text);
+//else if statements used to execute if condition1 is false and condition2 is true
+let greeting;
+if (hours < 10) {
+  greeting = "Good morning";
+} else if (hours < 20) {
+  greeting = "Good day";
+} else {
+  greeting = "Good evening";
+}
+console.log (greeting);
