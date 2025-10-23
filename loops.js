4dd4765fc66eb729b43564d46b388480d563f0f5
Evans Karuri <duneevanson6@gmail.com>
2025-10-23 15:25:21 -0400

    while and do...while loops

--- loops.js
+++ loops.js
@@ -1,4 +1,19 @@
+/* already done this part
 //for loop loops until the specified condition is false
 for (let i = 0; i <= 5; i++) {
   console.log("The number is " + i);
+}
+  */
+// do...while loop will execute the code block once, before checking if the condition is true, then it will repeat the loop as long as the condition is true.
+let j = 3;
+do {
+    console.log ("The number is "+ j);
+    j++;
+}
+while (j < 10);
+//while loops Loops through a block of code as long as a specified condition is true.
+let i = 20;
+while (i < 25){
+    console.log ("The number is "+ i);
+    i++;
 }
