426ffcf53ca3948a83d1b5163c15d3e208fe07fd
Evans Karuri <duneevanson6@gmail.com>
2025-10-23 16:30:48 -0400

    functio declaration and function calling exercise.

---
+++ function_exercise.js
@@ -0,0 +1,9 @@
+//Write a function that takes two numbers as parameters and returns their sum.
+function sum (num1,num2) {
+    return num1 + num2; //sum of the two numbers
+}
+// 3. Call the function with two numbers and store the result
+let result = sum(5, 7);
+
+// 4. Print the result to the console
+console.log(result);
