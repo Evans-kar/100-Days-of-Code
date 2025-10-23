50c3911f998510dd7170ea2227d1f5648f959663
Evans Karuri <duneevanson6@gmail.com>
2025-10-23 16:04:48 -0400

    aced the conditional practice exercise

---
+++ conditional_exercise.js
@@ -0,0 +1,9 @@
+//Exercise 1: Conditional Logic Practice
+let number = 10 //Declare a variable named `number` and initialize it with a value.
+if (number > 10) {
+    console.log("number is positive")
+} else if(number < 10) {
+    console.log ("number is negative")
+} else {
+    console.log ("the number is 10")
+}
---
+++ functions.js
@@ -0,0 +1,7 @@
+//introductions to functions, function declaration and function initialization in js
+//Functions are the main “building blocks” of the program. They allow the code to be called many times without repetition.
+//function declaration
+function showMessage (){
+    console.log("Hey Luffy, keep trying a day at a time till you get there don't you dare give up you got this!!")
+}
+showMessage () //function call
